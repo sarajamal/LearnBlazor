bbbc63aaa8c5af702efccad5218c6eab4c022e9e
سارة جمال <soso_j_kh@hotmail.com>
2024-08-14 15:16:16 +0300

    #AddbindProp-Demopeop

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public double price { get; set; }
+        public string Name { get; set; }
+        public bool isActive { get; set; }
+        public IEnumerable<Product__prop> ProductProperies { get; set; }
+
+    }
+}
