b996f57cdbef091c932faa758c3b7059293bc699
سارة جمال <soso_j_kh@hotmail.com>
2024-09-03 10:26:38 +0300

    Blazor&JavaScripr

---
+++ Helper/JSRunTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class JSRunTimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime JsRunTime , string message )
+        {
+            await JsRunTime.InvokeVoidAsync("showToastr", "success", message);
+        }
+        public static async Task ToastrEorros(this IJSRuntime JsRunTime, string message)
+        {
+            await JsRunTime.InvokeVoidAsync("showToastr", "error", message);
+        }
+
+    }
+}
---
+++ wwwroot/js/common.js
@@ -0,0 +1,26 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message)
+    }
+    if (type == "error") {
+        toastr.error(message)
+    }
+}
+
+window.showSWT = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message,
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    }
+}
